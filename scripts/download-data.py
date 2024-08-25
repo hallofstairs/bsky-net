0c974e339ebbdb30d4d24ab16abee04792940f36
Jett Hollister <jett.hollister@duke.edu>
2024-08-25 15:09:15 -0400

    Script for downloading dataset

---
+++ scripts/download-data.py
@@ -0,0 +1,21 @@
+"""Download the bluesky dataset from Hugging Face."""
+
+import os
+
+from huggingface_hub import snapshot_download
+
+HF_REPO_ID = "hallofstairs/bluesky"
+TARGET_LOCAL_DIR = "data/raw/test"
+
+if __name__ == "__main__":
+    os.makedirs(TARGET_LOCAL_DIR, exist_ok=True)
+    print("Downloading bluesky dataset from Hugging Face...")
+
+    snapshot_download(
+        repo_id=HF_REPO_ID,
+        repo_type="dataset",
+        local_dir=TARGET_LOCAL_DIR,
+        ignore_patterns=["README.md", ".gitattributes"],
+    )
+
+    print(f"Download complete. File saved to: {TARGET_LOCAL_DIR}")
