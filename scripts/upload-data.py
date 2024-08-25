3f1102da81604af815df877212326c8ba2b2c2bf
Jett Hollister <jett.hollister@duke.edu>
2024-08-25 15:27:07 -0400

    Upload dataset script

---
+++ scripts/upload-data.py
@@ -0,0 +1,21 @@
+"""Upload the bluesky dataset to Hugging Face (only works if you have the credentials)"""
+
+import os
+
+from huggingface_hub import upload_folder
+
+HF_REPO_ID = "hallofstairs/bluesky"
+HF_TOKEN = os.getenv("HF_TOKEN")
+TARGET_LOCAL_DIR = "data/raw/stream-2023-07-01"
+
+if __name__ == "__main__":
+    print("Uploading bluesky dataset to Hugging Face...")
+
+    upload_folder(
+        repo_id=HF_REPO_ID,
+        folder_path=TARGET_LOCAL_DIR,
+        repo_type="dataset",
+        token=HF_TOKEN,
+    )
+
+    print("Upload complete.")
