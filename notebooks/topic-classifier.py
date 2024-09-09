1ebf318efbde387d821966dc2a79c97bff25f69a
Jett Hollister <jett.hollister@duke.edu>
2024-09-09 18:35:08 -0400

    Edits

--- notebooks/topic-classifier.py
+++ notebooks/topic-classifier.py
@@ -3,9 +3,11 @@
 import json
 import math
 import os
+import time
 
 from dotenv import load_dotenv
 from openai import OpenAI
+from openai.types.batch import Batch
 
 from bsky_net import Post, records
 
@@ -15,165 +17,63 @@ load_dotenv()
 # Constants
 STREAM_DIR = "../data/raw/stream-2023-07-01"
 LABELS_DIR = "../data/processed/topic-labels"
-END_DATE = "2023-05-01"
+BATCH_DIR = "../data/batches"
 
-# %%
+BATCH_MAX_N = 50_000
+BATCH_QUEUE_MAX_TOKENS = 1_900_000
+
+AVG_TOKENS_PER_POST = 90
+SYSTEM_PROMPT_TOKENS = 174
+AVG_TOKENS_PER_REQ = SYSTEM_PROMPT_TOKENS + AVG_TOKENS_PER_POST
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(api_key=OPENAI_API_KEY)
 
-# %% Test simple classifier
-
-TOPICS = ["moderation policy", "unrelated"]
+# %% Prompts
 
 
-# TODO: Add few-shot examples
 def make_prompt(post: Post):
     return f"""Classify the following:
 Text: "{post["text"]}"
-Topic:"""
+Class:"""
 
 
-zeroshot_prompt = """You are a Bluesky post classifier that identifies any posts talking specifically about *bluesky's* moderation policies. Only respond with the topic, either "moderation policy" or "unrelated"."""
+sys_prompt = """You are tasked with analyzing a post from the social network Bluesky to determine if it references the network's moderation policies, especially in relation to the Bluesky development team.
 
-fewshot_prompt = """You are a Bluesky post classifier that identifies any posts talking about Bluesky's moderation policies. Only respond with the topic, nothing else. Here are a few examples:
+Examples:
 Text: "This is definitely the answer. Everyone ranting and raving about moderation decisions here are completely blind to how things will work once federation launches."
-Topic: moderation policy
+Class: 1
 
-Text: "Woah this is really smart. I assumed they would drop the invites, but this makes trust and safety easier."
-Topic: moderation policy
+Text: "I've seen that Alice chick 4 times now. \n\nJust bury her and block. I thought that was the plan here"
+Class: 1
+
+Text: "I think I\u2019ve been on here a month with zero invite codes bluesky mods hate me"
+Class: 0
 
 Text: "So wait does "What's Hot Classic" mean the classic feed with nudes or without? Asking for a friend who is in horny jail."
-Topic: unrelated"""
+Class: 0"""
 
 
-post_count = 0
+# %% Create BatchAPI request
 
-for record in records(stream_dir=STREAM_DIR, end_date=END_DATE):
-    if record["$type"] == "app.bsky.feed.post":
-        # Ignore replies (for now)
-        if "reply" in record and record["reply"]:
-            continue
+BATCH_DESCRIPTION = "moderation-2023-05-24_2023-05-28"
 
-        # Ignore embeds (for now)
-        if "embed" in record and record["embed"]:
-            continue
+START_DATE = "2023-05-24"
+END_DATE = "2023-05-28"
 
-        post = Post(**record)
-        res = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                    "role": "system",
-                    "content": fewshot_prompt,
-                },
-                {"role": "user", "content": make_prompt(post)},
-            ],
-            logprobs=True,
-            temperature=0.0,
-        )
+if os.path.exists(f"{BATCH_DIR}/{BATCH_DESCRIPTION}"):
+    raise ValueError(f"Batch folder '{BATCH_DESCRIPTION}' already exists.")
 
-        classification = res.choices[0].message.content
+os.makedirs(f"{BATCH_DIR}/{BATCH_DESCRIPTION}")
 
-        if not classification:
-            print("No classification")
-            continue
-
-        topic = classification.strip().lower()
-        logprobs = res.choices[0].logprobs
-
-        probability = (
-            math.exp(logprobs.content[0].logprob)
-            if logprobs and logprobs.content
-            else -1
-        )
-
-        log_entry = {
-            "text": post["text"],
-            "classification": topic,
-            "probability": probability,
-            "uri": post["uri"],
-        }
-
-        with open(
-            f"{LABELS_DIR}/moderation-testing-4o-mini-fewshot.jsonl", "a"
-        ) as log_file:
-            json.dump(log_entry, log_file)
-            log_file.write("\n")
-
-        post_count += 1
-
-        if post_count == 1000:
-            break
-
-# %% Compare models
-
-total_diffs = 0
-
-with open(f"{LABELS_DIR}/moderation-testing-4o-mini-zeroshot.jsonl", "r") as f1, open(
-    f"{LABELS_DIR}/moderation-testing-4o-mini-fewshot.jsonl", "r"
-) as f2:
-    for model_1, model_2 in zip(f1, f2):
-        model_1_data = json.loads(model_1)
-        model_2_data = json.loads(model_2)
-
-        # if model_1_data["classification"] != model_2_data["classification"]:
-        #     total_diffs += 1
-        #     print(
-        #         model_1_data["text"],
-        #         model_1_data["classification"],
-        #         model_1_data["probability"],
-        #         model_2_data["classification"],
-        #         model_2_data["probability"],
-        #     )
-        #     print("---")
-
-        if model_1_data["classification"] == "moderation policy":
-            print(model_1_data["text"])
-
-    # print(f"Total diffs: {total_diffs}")
-
-
-# %% BatchAPI request
-
-# ~75 input tokens per post (~0.005625 / 1k posts), ~2 output tokens per post (~0.0006 / 1k posts)
-# Total cost: ~0.006225 / 1k posts
-# 50k posts: $0.31
-# 100k posts: $0.62
-# 1m posts: $6.22
-
-
-BATCH_MAX_N = 50_000
-BATCH_QUEUE_MAX_TOKENS = 1_500_000
-
-BATCH_DIR = "../data/batches"
-BATCH_DESCRIPTION = "moderation-spike-4o-mini-zeroshot"
-
-AVG_TOKENS_PER_POST = 30.7
-INPUT_PROMPT_TOKENS = 40
-AVG_TOKENS_PER_REQ = INPUT_PROMPT_TOKENS + AVG_TOKENS_PER_POST
-
-if os.path.exists(f"{BATCH_DIR}/{BATCH_DESCRIPTION}.jsonl"):
-    raise ValueError(f"Batch file '{BATCH_DESCRIPTION}.jsonl' already exists.")
-
+i = 0
 batch_size = 0
 total_tokens = 0
-last_uri = ""
 
-# at://did:plc:j53elsjlcag3iuran5ezcy32/app.bsky.feed.post/3jw7y37h7a427
-# at://did:plc:pt5p4naogbcoxqnin32kwuoc/app.bsky.feed.post/3jwj6dxfptc2v
-# at://did:plc:yteysv5adlllg4e3xek3e3uk/app.bsky.feed.post/3jwjcpuvhy22r
-# Started around here (May 25): at://did:plc:4t2ziwnnescprzorvmrfduey/app.bsky.feed.post/3jwjhkteeq22f
-
-# TODO: Convert all \u2019 and \u2018 to '
-
-# Create BatchAPI jsonl file
-for record in records(
-    stream_dir=STREAM_DIR, start_date="2023-05-25", end_date="2023-05-29"
-):
+# Create batch files based on limits
+for record in records(stream_dir=STREAM_DIR, start_date=START_DATE, end_date=END_DATE):
     if record["$type"] == "app.bsky.feed.post":
-        post = Post(**record)
+        post: Post = record
 
         # Ignore replies (for now)
         if "reply" in post and post["reply"]:
@@ -190,7 +90,7 @@ for record in records(
             "body": {
                 "model": "gpt-4o-mini",
                 "messages": [
-                    {"role": "system", "content": zeroshot_prompt},
+                    {"role": "system", "content": sys_prompt},
                     {"role": "user", "content": make_prompt(post)},
                 ],
                 "logprobs": True,
@@ -198,50 +98,106 @@ for record in records(
             },
         }
 
-        with open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}.jsonl", "a") as f:
+        batch_path = f"{BATCH_DIR}/{BATCH_DESCRIPTION}/{i}.jsonl"
+        with open(batch_path, "a") as f:
             json.dump(batch_obj, f)
             f.write("\n")
 
         batch_size += 1
         total_tokens += AVG_TOKENS_PER_REQ
-        last_uri = post["uri"]
 
-        if batch_size == BATCH_MAX_N:
-            print(
-                f"Batch max posts reached: {batch_size}. Total tokens: {total_tokens}"
-            )
-            break
-
-        if total_tokens > BATCH_QUEUE_MAX_TOKENS:
-            print(
-                f"Batch queue max tokens reached: {total_tokens}. Total posts: {batch_size}"
-            )
-            break
+        if batch_size == BATCH_MAX_N or total_tokens > BATCH_QUEUE_MAX_TOKENS:
+            print(f"Batch {i} saved: {batch_size} posts, {total_tokens} tokens")
+            i += 1
+            batch_size = 0
+            total_tokens = 0
 
 print(f"Total posts: {batch_size}")
 print(f"Total tokens: {total_tokens}")
-print(
-    f"Last URI: {last_uri}"
-)  # at://did:plc:2px2wmhufv4jzdolr5fjawmv/app.bsky.feed.post/3jwlp3dllkk2q
 
 
-# %% Create BatchAPI request
+# %% Push batch files to Batch API
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+idx = 0
 
 batch_input_file = client.files.create(
-    file=open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}.jsonl", "rb"),
+    file=open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}/{idx}.jsonl", "rb"),
     purpose="batch",
 )
 
-client.batches.create(
+batch = client.batches.create(
     input_file_id=batch_input_file.id,
     endpoint="/v1/chat/completions",
     completion_window="24h",
-    metadata={"description": BATCH_DESCRIPTION},
+    metadata={"description": f"{BATCH_DESCRIPTION}-{idx}"},
 )
+
 
 # %% Check on batch
 
-print(client.batches.retrieve("batch_Q47CZsPZEutytV7Sw7wAR9lL"))
+idx = 0
+
+batch_completed = False
+batch_id: str | None = None
+batch: Batch | None = None
+
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def push_batch(idx: int):
+    batch_input_file = client.files.create(
+        file=open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}/{idx}.jsonl", "rb"),
+        purpose="batch",
+    )
+
+    batch = client.batches.create(
+        input_file_id=batch_input_file.id,
+        endpoint="/v1/chat/completions",
+        completion_window="24h",
+        metadata={"description": f"{BATCH_DESCRIPTION}-{idx}"},
+    )
+
+    return batch
+
+
+while True:
+    if not batch:
+        try:
+            batch = push_batch(idx)
+        except Exception as e:
+            print(e)
+            break
+
+    batch_info = client.batches.retrieve(batch.id)
+
+    if batch_info.completed_at:
+        output = client.files.content(batch_info.id)
+
+        with open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}/output-{idx}.jsonl", "w") as f:
+            f.write(output.text)
+
+        print(f"Batch {idx} completed and results saved.")
+        batch = None
+        idx += 1
+
+    time.sleep(60)
+
+# %%
+
+batch_input_file = client.files.create(
+    file=open(f"{BATCH_DIR}/{BATCH_DESCRIPTION}/{1}.jsonl", "rb"),
+    purpose="batch",
+)
+
+batch = client.batches.create(
+    input_file_id=batch_input_file.id,
+    endpoint="/v1/chat/completions",
+    completion_window="24h",
+    metadata={"description": f"{BATCH_DESCRIPTION}-{1}"},
+)
 
 # %% Map results
 
@@ -305,35 +261,3 @@ with open(INPUT_PATH, "r") as inputs, open(OUTPUT_PATH, "r") as outputs:
         count += 1
 
 print(f"Total posts: {count}")
-
-# %% Looking for start of moderation issue in hellthread
-
-for record in records(
-    "../data/raw/stream-2023-07-01", start_date="2023-05-24", end_date="2023-5-25"
-):
-    if record["$type"] == "app.bsky.feed.post":
-        if "@pfrazee.com" in record["text"]:
-            print(record["text"])
-
-# %% Prompt testing
-
-
-# %% Splitting up Batch API requests
-
-BATCH_MAX_N = 50_000
-BATCH_QUEUE_MAX_TOKENS = 1_950_000
-
-BATCH_DIR = "../data/batches"
-EXPERIMENT_NAME = "moderation-4o-mini-fewshot-v1"
-
-AVG_TOKENS_PER_POST = 30.7
-INPUT_PROMPT_TOKENS = 40
-AVG_TOKENS_PER_REQ = INPUT_PROMPT_TOKENS + AVG_TOKENS_PER_POST
-
-
-for record in records(
-    "../data/raw/stream-2023-07-01", start_date="2023-05-24", end_date="2023-5-25"
-):
-    if record["$type"] == "app.bsky.feed.post":
-        if "@pfrazee.com" in record["text"]:
-            print(record["text"])
