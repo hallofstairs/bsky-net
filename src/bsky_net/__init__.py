ead1497c74aaf0b283ac442fc5fd5ac34f38e523
Jett Hollister <jett.hollister@duke.edu>
2024-10-28 16:52:49 -0400

    Consolidation

--- src/bsky_net/__init__.py
+++ src/bsky_net/__init__.py
@@ -4,7 +4,12 @@ import time
 import typing as t
 from datetime import datetime, timedelta
 
+from openai.types.shared_params.response_format_json_schema import JSONSchema
+
 T = t.TypeVar("T")
+
+
+# === Iteration utils ===
 
 
 class jsonl[T]:
@@ -17,6 +22,33 @@ class jsonl[T]:
                 except json.JSONDecodeError:
                     print(f"JSONDecodeError: {line}")
                     continue
+
+
+def records(
+    stream_path: str = "../data/raw/stream-2023-07-01",
+    start_date: str = "2022-11-17",
+    end_date: str = "2023-05-01",
+    log: bool = True,
+) -> t.Generator["Record", None, None]:
+    """
+    Generator that yields records from the stream for the given date range.
+
+    End date is inclusive.
+    """
+
+    def generate_timestamps(start_date: str, end_date: str) -> list[str]:
+        start_dt = datetime.strptime(start_date, "%Y-%m-%d")
+        end_dt = datetime.strptime(end_date, "%Y-%m-%d")
+        delta = end_dt - start_dt
+
+        return [
+            (start_dt + timedelta(days=i)).strftime("%Y-%m-%d")
+            for i in range(delta.days + 1)
+        ]
+
+    for ts in tq(generate_timestamps(start_date, end_date), active=log):
+        for record in jsonl[Record].iter(f"{stream_path}/{ts}.jsonl"):
+            yield record
 
 
 def tq(iterable: t.Iterable[T], active: bool = True) -> t.Generator[T, None, None]:
@@ -42,6 +74,9 @@ def tq(iterable: t.Iterable[T], active: bool = True) -> t.Generator[T, None, Non
                 sys.stdout.flush()
 
         yield item
+
+
+# === Data types ===
 
 
 class CidUri(t.TypedDict):
@@ -118,32 +153,7 @@ Profile = t.TypedDict(
 Record = Post | Follow | Repost | Like | Block | Profile
 
 
-def generate_timestamps(start_date: str, end_date: str) -> list[str]:
-    start_dt = datetime.strptime(start_date, "%Y-%m-%d")
-    end_dt = datetime.strptime(end_date, "%Y-%m-%d")
-    delta = end_dt - start_dt
-
-    return [
-        (start_dt + timedelta(days=i)).strftime("%Y-%m-%d")
-        for i in range(delta.days + 1)
-    ]
-
-
-def records(
-    stream_path: str = "../data/raw/stream-2023-07-01",
-    start_date: str = "2022-11-17",
-    end_date: str = "2023-05-01",
-    log: bool = True,
-) -> t.Generator[Record, None, None]:
-    """
-    Generator that yields records from the stream for the given date range.
-
-    End date is inclusive.
-    """
-
-    for ts in tq(generate_timestamps(start_date, end_date), active=log):
-        for record in jsonl[Record].iter(f"{stream_path}/{ts}.jsonl"):
-            yield record
+# === Data utils ===
 
 
 def did_from_uri(uri: str) -> str:
@@ -154,3 +164,75 @@ def did_from_uri(uri: str) -> str:
         return uri.split("/")[2]
     except Exception:
         raise ValueError(f"\nMisformatted URI: {uri}")
+
+
+# === Prompts ===
+
+
+class Prompts:
+    class Topic:
+        SYSTEM: str = """You are an NLP expert, tasked with performing opinion mining on posts from the Bluesky social network. Your goal is to detect the post author's opinion on the Bluesky team's approach thus far to moderation and trust and safety (T&S) on their platform. 
+
+If the post is unrelated to moderation on Bluesky, indicate that the post is 'off-topic'.
+
+Include quotes from the text that you used to reach your answer."""
+
+        OUTPUT_SCHEMA: JSONSchema = {
+            "name": "opinion_mining",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "required": ["reasoning", "on_topic"],
+                "additionalProperties": False,
+                "properties": {
+                    "reasoning": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "required": ["quote", "conclusion"],
+                            "properties": {
+                                "quote": {"type": "string"},
+                                "conclusion": {"type": "string"},
+                            },
+                            "additionalProperties": False,
+                        },
+                    },
+                    "on_topic": {"type": "boolean"},
+                },
+            },
+        }
+
+    class Stance:
+        SYSTEM: str = """You are an NLP expert, tasked with performing stance detection on posts from the Bluesky social network. Your goal is to detect the post author's stance on the Bluesky team's approach to moderation and trust and safety (T&S) on their platform.
+
+If the post is unrelated to social media moderation on Bluesky specifically, indicate that the post is 'off-topic'.
+
+If the post is on-topic, classify the stance of the author on the BLUESKY TEAM's MODERATION EFFORTS as defending/sympathizing with them (favor), criticizing them (against), or neither (none). If the post's opinion is not directed towards the BLUESKY TEAM's moderation approach, even if it's on-topic, indicate that the opinion is 'none'.
+
+Include quotes from the text that you used to reach your answer."""
+
+        OUTPUT_SCHEMA: JSONSchema = {
+            "name": "stance_detection",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "required": ["reasoning", "on_topic", "opinion"],
+                "additionalProperties": False,
+                "properties": {
+                    "reasoning": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "required": ["quote", "conclusion"],
+                            "properties": {
+                                "quote": {"type": "string"},
+                                "conclusion": {"type": "string"},
+                            },
+                            "additionalProperties": False,
+                        },
+                    },
+                    "on_topic": {"type": "boolean"},
+                    "opinion": {"enum": ["favor", "against", "none"], "type": "string"},
+                },
+            },
+        }
