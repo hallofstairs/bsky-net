540f72fdcdbeeaca382bbe9a50cbe53360bab7f8
Jett Hollister <jett.hollister@duke.edu>
2024-11-04 15:37:42 -0500

    Switched to HF's upload_large_folder

--- scripts/upload-data.py
+++ scripts/upload-data.py
@@ -1,21 +1,17 @@
 """Upload the bluesky dataset to Hugging Face (only works if you have the credentials)"""
 
-import os
+from huggingface_hub import upload_large_folder
 
-from huggingface_hub import upload_folder
-
-HF_REPO_ID = "hallofstairs/bluesky"
-HF_TOKEN = os.getenv("HF_TOKEN")
-TARGET_LOCAL_DIR = "data/raw/bluesky"
+HF_REPO_ID = "hallofstairs/bsky-net"
+TARGET_LOCAL_DIR = "data/processed/bsky-net-daily"
 
 if __name__ == "__main__":
     print("Uploading bluesky dataset to Hugging Face...")
 
-    upload_folder(
+    upload_large_folder(
         repo_id=HF_REPO_ID,
         folder_path=TARGET_LOCAL_DIR,
         repo_type="dataset",
-        token=HF_TOKEN,
     )
 
     print("Upload complete.")
